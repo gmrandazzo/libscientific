951b9688f6e78808b1cef1825cb35290290f849f
Giuseppe Marco Randazzo <gmrandazzo@gmail.com>
2016-07-13 15:47:57 +0200

    Clean

---
+++ build/src/CMakeFiles/testarray.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testarray.dir/testarray.c.o"
+  "testarray.pdb"
+  "testarray"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testarray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/testgraphs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testgraphs.dir/testgraphs.c.o"
+  "testgraphs.pdb"
+  "testgraphs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testgraphs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
